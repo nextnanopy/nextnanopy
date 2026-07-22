1a5b8009650c2e01d3b53462ea25361e88be518e
Heorhii nn-mu <heorhii.yehiazarian@nextnano.com>
2026-07-22 14:41:57 +0200

    fix path in template

--- templates/execute_input_file.py
+++ templates/execute_input_file.py
@@ -2,7 +2,7 @@ import nextnanopy as nn
 from pathlib import Path
 
 this_dir = Path(__file__).parent
-input_file_path = this_dir / "input_files" / "DoubleQuantumWell_6nm_demo.nnp"
+input_file_path = this_dir / "input files" / "DoubleQuantumWell_6nm_demo.nnp"
 
 
 # execute input file
