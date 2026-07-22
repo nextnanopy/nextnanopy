eae379d2fb77eaddb202956b5562957903a61a60
Heorhii nn-mu <heorhii.yehiazarian@nextnano.com>
2026-07-22 15:12:20 +0200

    add execution of test input file to the workflow

--- templates/execute_input_file.py
+++ templates/execute_input_file.py
@@ -8,5 +8,9 @@ input_file_path = this_dir / "input files" / "DoubleQuantumWell_6nm_demo.nnp"
 # execute input file
 input_file = nn.InputFile(input_file_path)
 
-input_file.execute()
+# by default the failed simulation does not raise an exception
+# but you can change this behavior with the `convergenceCheck` argument
+input_file.execute(convergenceCheck=True, convergence_check_mode="terminate")
+
+
 
