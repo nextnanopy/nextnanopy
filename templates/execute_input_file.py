6f06b0f7918c459d331b06abab0859513e939814
Heorhii nn-mu <heorhii.yehiazarian@nextnano.com>
2026-07-22 14:41:22 +0200

    add another example to templates

---
+++ templates/execute_input_file.py
@@ -0,0 +1,12 @@
+import nextnanopy as nn
+from pathlib import Path
+
+this_dir = Path(__file__).parent
+input_file_path = this_dir / "input_files" / "DoubleQuantumWell_6nm_demo.nnp"
+
+
+# execute input file
+input_file = nn.InputFile(input_file_path)
+
+input_file.execute()
+
