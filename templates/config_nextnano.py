df21a2ff734ed65c23def7b2f70136cca21d2f50
Heorhii nn-mu <heorhii.yehiazarian@nextnano.com>
2026-07-22 20:59:48 +0200

    update config*.py

--- templates/config_nextnano.py
+++ templates/config_nextnano.py
@@ -16,7 +16,7 @@ path_nextnano_output = r"C:\Users\homer.simpson\Documents\nextnano\Output"
 #++++++++++++++++++++++++++++++++++++++++++++++++++
 # Specify your nextnano installation folder
 #++++++++++++++++++++++++++++++++++++++++++++++++++
-path_nextnano        = r"C:\Program Files\nextnano\2025_05_09"           # nextnano++, nextnano3 and nextnano.MSB software            
+path_nextnano        = r"C:\Program Files\nextnano\2026_07_03"           # nextnano++, nextnano3, nextnano.NEGF and nextnano.MSB software            
 
 
 # NO NEED TO CHANGE THE FOLLOWING -----------------------------
@@ -35,31 +35,33 @@ nn.config.set('nextnano.MSB' , 'outputdirectory', path_nextnano_output)
 #---------------------------
 nn.config.set('nextnano++'   , 'license', os.path.join(path_license, r'License_nnp.lic'))
 nn.config.set('nextnano3'    , 'license', os.path.join(path_license, r'License_nnp.lic'))
+nn.config.set('nextnano.MSB' , 'license', os.path.join(path_license, r'License_nnp.lic'))
 nn.config.set('nextnano.NEGF', 'license', os.path.join(path_license, r'License_nnNEGF.lic'))
+nn.config.set('nextnanoevo', 'license',   os.path.join(path_license, r'License_nnevo.lic'))
 
 #----------------------------------------------------------
 # Location of nextnano++ files:    executable and database
 #----------------------------------------------------------
 nn.config.set('nextnano++', 'exe'     , os.path.join(path_nextnano, r'nextnano++\bin\nextnano++_Intel_64bit.exe'))
-nn.config.set('nextnano++', 'database', os.path.join(path_nextnano, r'nextnano++\database\database_nnp.in'))
+nn.config.set('nextnano++', 'database', os.path.join(path_nextnano, r'nextnano++\database\database.nnp'))
 
 #----------------------------------------------------------
 # Location of nextnano3 files:     executable and database
 #----------------------------------------------------------
 nn.config.set('nextnano3', 'exe'     , os.path.join(path_nextnano, r'nextnano3\bin\nextnano3_Intel_ifx.exe'))
-nn.config.set('nextnano3', 'database', os.path.join(path_nextnano, r'nextnano3\database\database_nn3.in'))
+nn.config.set('nextnano3', 'database', os.path.join(path_nextnano, r'nextnano3\database\database.nn3'))
 
 #----------------------------------------------------------
 # Location of nextnano.NEGF files: executable and database
 #----------------------------------------------------------
 nn.config.set('nextnano.NEGF', 'exe'     , os.path.join(path_nextnano, r'nextnano.NEGF\bin\nextnano.NEGF_win.exe'))
-nn.config.set('nextnano.NEGF', 'database', os.path.join(path_nextnano, r'nextnano.NEGF\database\Material_Database.in'))
+nn.config.set('nextnano.NEGF', 'database', os.path.join(path_nextnano, r'nextnano.NEGF\database\Material_Database.negf'))
 
 #----------------------------------------------------------
 # Location of nextnano.MSB files: executable and database
 #----------------------------------------------------------
-nn.config.set('nextnano.MSB', 'database', os.path.join(path_nextnano, r'nextnano.MSB\Materials.xml'))
-
+nn.config.set('nextnano.MSB', 'database', os.path.join(path_nextnano, r'nextnano.MSB\database\materials.msb'))
+nn.config.set('nextnano.MSB', 'exe'     , os.path.join(path_nextnano, r'nextnano.MSB\bin\nextnano.MSB_Intel.exe'))
 nn.config.save() # save modifications
 
 print("The nextnanopy config file has been updated and saved.")
